8a7cd8cdec0380e4b2c31730ab1427b39c1cd294
ho980719 <ho980719@gmail.com>
2023-07-20 17:00:21 +0900

    프로그래머스 코테

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,64 @@
-import java.sql.Array;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        String pos = "A7";
+        char[] arr = new char[8];
 
-    // https://school.programmers.co.kr/learn/courses/30/lessons/150370
-    public static void main(String[] args) throws Exception {
-        int[] answer = {};
-        int answerIndex = 0;
+        int[][] chess = new int[8][8];
 
-        List<Integer> result = new ArrayList<>();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
-
-        String today = "2022.05.19";
-        String[] terms = {"A 6", "B 12", "C 3"};
-        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
-
-        // today string to date
-        Date d = format.parse(today);
-        // terms set
-        Map term = new HashMap();
-        for (String s : terms) {
-            term.put(s.split(" ")[0], s.split(" ")[1]);
+        int index = 0;
+        for (int i = 'A'; i <= 'H'; i++) {
+            arr[index] = (char) i;
+            index++;
         }
 
-        for (int i = 0; i < privacies.length; i++) {
-            String insertDate = privacies[i].split(" ")[0];
-            String type = privacies[i].split(" ")[1];
+        int charIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == pos.charAt(0))
+                charIndex = i;
+            System.out.print(arr[i] + " ");
+        }
 
-            Date date = format.parse(insertDate);
-            Calendar c = Calendar.getInstance();
-            c.setTime(date);
+        Position position = new Position();
+        position.x = 0 + charIndex;
+        position.y = 7 - (Integer.parseInt(String.valueOf(pos.charAt(1))) - 1);
 
-            c.add(Calendar.MONTH, Integer.parseInt(String.valueOf(term.get(type))));
-            Date exDate = c.getTime();
-            if (exDate.before(d)) {
-                // 파기
-                result.add(i+1);
+        System.out.println("[" + position.x + "," + position.y + "]");
+
+        chess[position.y][position.x] = 1;
+
+        int answer = 0;
+
+        if (position.x + 2 < 8) {
+            if (position.y + 1 < 8) answer++;
+            if (position.y - 1 >= 0) answer++;
+        }
+        if (position.x - 2 >= 0) {
+            if (position.y + 1 < 8) answer++;
+            if (position.y - 1 >= 0) answer++;
+        }
+        if (position.y + 2 < 8) {
+            if (position.x + 1 < 8) answer++;
+            if (position.x - 1 >= 0) answer++;
+        }
+        if (position.y - 2 >= 0) {
+            if (position.x + 1 < 8) answer++;
+            if (position.x - 1 >= 0) answer++;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(chess[i][j] + " ");
             }
+            System.out.println("");
         }
 
-        answer = new int[result.size()];
+        System.out.println(answer);
+    }
 
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
-
-        for (int i : answer) {
-            System.out.print(i + " ");
-        }
-
+    static class Position {
+        int x = 0;
+        int y = 0;
     }
 }
--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -7,8 +7,32 @@ public class Sort {
     public static void main(String[] args) throws Exception {
         int[] test = {18, 10, 11, 13, 17};
 
-        for (int n : bubbleSort(test))
-            System.out.print(n + " ");
+        // 선택정렬
+//        for (int n : selectSort(test, 0))
+//            System.out.print(n + " ");
+        quickSort(test, 0, test.length - 2, test.length - 1);
+        System.out.println();
+
+
+    }
+
+    public static int[] quickSort(int[] arr, int start, int end, int pivot) {
+        // 상대적 구현 난이도 어려움.
+        // 시간복잡도 = O(nlogn), 최악의 경우 O(n^2)
+        // 기준값(pivot)을 선정, 해당 값보다 작은 데이터와 큰 데이터로 분류, 반복 정렬
+        // 기준값이 어떻게 선정되는지에 따라 시간 복잡도에 많은 영향을 준다.
+
+        // start 값이 pivot 보다 클경우 우측으로 정렬
+        if (arr[pivot] < arr[start]) {
+            quickSort(arr, start + 1, end, pivot);
+        }
+        // end 값이 pivot 보다 작을경우 좌측으로 정렬
+        if (pivot > end) {
+            quickSort(arr, start, end - 1, pivot);
+        }
+        // start 값이 pivot 보다 크고, end 값이 pivot 보다 작을경우 swap
+
+        return arr;
     }
 
     public static int[] bubbleSort(int[] arr) {
@@ -18,7 +42,6 @@ public class Sort {
         // 시간복잡도 = O(n^2)
         for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                System.err.println(j);
                 int c = 0;
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j + 1];
@@ -31,4 +54,54 @@ public class Sort {
         }
         return arr;
     }
+
+    public static int[] selectSort(int[] arr, int order) {
+        // 상대적 구현 난이도 어려움.
+        // 비효율적임
+        // 시간복잡도 = O(n^2)
+        // 원리는 매우 간단
+
+        // 1. 탐색 범위 지정
+        // 2. 최솟값 또는 최댓값을 찾고 남은 정렬부분의 가장 앞에 있는 데이터와 swap
+
+        if (order == 0) {
+            // 내림차순
+            for (int i = 0; i < arr.length; i++) {
+                int max = Integer.MIN_VALUE;
+                int maxIndex = 0;
+                for (int j = i; j < arr.length; j++) {
+                    if (arr[j] > max) {
+                        max = arr[j];
+                        maxIndex = j;
+                    }
+
+                    if (j == arr.length - 1) {
+                        int temp = arr[i];
+                        arr[i] = arr[maxIndex];
+                        arr[maxIndex] = temp;
+                    }
+                }
+            }
+        } else if (order == 1) {
+            // 오름차순
+            for (int i = 0; i < arr.length; i++) {
+                int min = Integer.MAX_VALUE;
+                int minIndex = 0;
+                for (int j = i; j < arr.length; j++) {
+                    if (arr[j] < min) {
+                        min = arr[j];
+                        minIndex = j;
+                    }
+
+                    if (j == arr.length - 1) {
+                        int temp = arr[i];
+                        arr[i] = arr[minIndex];
+                        arr[minIndex] = temp;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+
 }
