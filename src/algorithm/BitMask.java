3d6d7773d14c05c0973789e1573cc2fb2482f652
ho980719 <ho980719@gmail.com>
2023-07-31 16:50:06 +0900

    bitmask

--- src/algorithm/BitMask.java
+++ src/algorithm/BitMask.java
@@ -10,6 +10,16 @@ public class BitMask {
         int B = (1 << 20) - 1;
         System.out.println(B);
         System.out.println(Integer.toBinaryString(B));
-        // 원소 추가
+        // 원소 삭제 (4를 제거)
+        System.out.println(Integer.toBinaryString(B &= ~(1 << 3)));
+        // 원소 추가 (B 집합 다시 4를 추가, A 공집합에 13 추가)
+        System.out.println(Integer.toBinaryString(B |= (1 << 3)));
+        System.out.println(Integer.toBinaryString(A |= (1 << 12)));
+        // 원소의 포함 여부 확인 (A에 4가 있냐, B에 4가 있냐)
+        System.out.println((A & (1 << 3)) == (1 << 3));
+        System.out.println((B & (1 << 3)) == (1 << 3));
+        // 원소의 토글 (있으면 삭제, 없으면 추가) -> 12
+        System.out.println(Integer.toBinaryString(A ^= (1 << 11)));
+        System.out.println(Integer.toBinaryString(B ^= (1 << 11)));
     }
 }
